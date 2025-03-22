0049e413629e8c198fac1d7666a3281bb7e866fd
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2025-03-22 11:31:35 +0100

    README

--- README.md
+++ README.md
@@ -1,24 +1,56 @@
 # Blog
+Una aplicación para gestionar un blog de una comunidad de usuarios.
+
+# Descripción
+
+La aplicación tiene las siguientes características:
+
+- Es instalable en PC y móvil, es decir es una aplicación web progresiva (PWA)
+- Permite el registro de múltiples usuarios  
+- Existen 2 roles de usuario: ADMIN y USER
+  
+
+La aplicación tiene las siguientes funcionalidades:
+
+- En la página inicial se muestran todos los posts publicados, que son públicos para todo el mundo.
+- Cualquier usuario registrado puede crear posts y modificar y eliminar los que ha creado.
+- Sólo el usuario con rol ADMIN puede publicar un post de cualquier usuario. También puede despublicar.
+- Los usuarios con rol USER no pueden publicar ni despublicar los post que han creado ni ningún otro.
+- Una vez publicado un post, no podrá editarse ni eliminarse. 
+- Solo el usuario con rol ADMIN podrá despublicar un post y así permitir la modificación o eliminación de dicho post.
+  
 
 
 
+# Detalles técnicos
 
-## Dependencias
+Si deseas probar por ti mismo el código, sigue los siguientes pasos.
 
-Se han instalado las siguientes dependencias:
+## Clonar repositorio
 
 ```
-npm  install  prisma  @faker-js/faker  -D  
-npm  install  @prisma/client  next-auth@beta  @auth/prisma-adapter 
-npm  install  lucide-react  bcryptjs  cloudinary  react-spinners  sonner  slugify
-npm  install  @tiptap/react  @tiptap/pm  @tiptap/html  @tiptap/starter-kit
-npm  install  @tiptap/extension-color
-npm  install  @tiptap/extension-list-item 
-npm  install  @tiptap/extension-text-style
+git  clone  https://github.com/jamj2000/nxapp-blog
+cd   nxapp-blog
 ```
+
+## Configurar variables de entorno
+
+```
+mv  .env.example  .env
+```
+Y establecemos valores para las variables.
+
+
+## Instalar dependencias
+
+ ```
+ npm  install
+ ```
 
 
 ## Inicializar BD (seed)
+
+Para inicializar la BD con datos ficticios hacemos
 
 ```
 npm  run  seed
@@ -26,9 +58,40 @@ npm  run  seed
 
 ## Iniciar en modo desarrollo
 
+
 ```
 npm  run  dev
 ```
+
+
+## Dependencias
+
+Este proyecto hace uso de las siguientes dependencias:
+
+**Dependencias de desarrollo**
+
+- prisma
+- @faker-js/faker 
+
+**Dependencias de aplicación**
+
+- @prisma/client  
+- next-auth@beta  
+- @auth/prisma-adapter 
+- lucide-react
+- bcryptjs
+- cloudinary
+- react-spinners
+- sonner
+- slugify
+- @tiptap/react  
+- @tiptap/pm  
+- @tiptap/html  
+- @tiptap/starter-kit
+- @tiptap/extension-color
+- @tiptap/extension-list-item 
+- @tiptap/extension-text-style
+
 
 ## Diagrama E-R simplificado
 
--- src/components/posts/lista.js
+++ src/components/posts/lista.js
@@ -1,6 +1,6 @@
 import { PAGE, PER_PAGE } from '@/lib/pagination'
 import { TrashIcon, SquareArrowOutUpRightIcon, EyeIcon, PencilIcon } from "lucide-react";
-import { getPostsWithCategory, getAllPosts } from '@/lib/data'
+import { getPostsWithCategory, getAllPosts, getAllPostsByAuthor } from '@/lib/data'
 import { auth } from "@/auth"
 import { publishPost } from '@/lib/actions';
 import Modal from '@/components/modal';
@@ -9,20 +9,32 @@ import PostModificar from '@/components/posts/modificar';
 import PostEliminar from '@/components/posts/eliminar';
 import PaginationControls from '@/components/pagination-control'
 import Link from 'next/link';
+import { redirect } from 'next/dist/server/api-utils';
 
 
 async function Posts({ searchParams }) {
     const session = await auth()
 
+    if (!session) redirect('/')
+
     const { page = PAGE, per_page = PER_PAGE, category = '' } = await searchParams
 
 
     let posts = []
-    if (category) {
-        posts = await getPostsWithCategory(category)
-    } else {
+    if (session.user?.role === 'ADMIN') {
         posts = await getAllPosts(page)
     }
+    else {
+        posts = await getAllPostsByAuthor(session.user?.id, page)
+    }
+
+    // if (category) {
+    //     posts = await getPostsWithCategory(category)
+    // } else {
+    //     posts = await getAllPosts(page)
+    // }
+
+
     // console.log(posts);
     // mocked, skipped and limited in the real app
     const start = (page - 1) * per_page // 0, 5, 10 ...
@@ -46,7 +58,8 @@ async function Posts({ searchParams }) {
                     <div key={post.id} className="p-1 flex justify-between items-center odd:bg-slate-100">
 
                         <div className='flex gap-1 items-center'>
-                            {session?.user?.role === 'ADMIN' &&
+
+                            {session.user?.role === 'ADMIN' &&
                                 <form action={publishPost.bind(null, post.id)}>
                                     <button
                                         className={`${post.is_draft ? 'bg-slate-300' : 'bg-slate-600'} p-2 rounded-full self-end hover:bg-slate-400`}
@@ -55,35 +68,52 @@ async function Posts({ searchParams }) {
                                     </button>
                                 </form>
                             }
+
                             <Link href={`/posts/${post.slug}`} className="font-bold cursor-pointer">{post.title}</Link>
                         </div>
 
-                        {session?.user?.role === 'ADMIN' &&
-                            <div className='flex justify-center items-center gap-1'>
 
-                                <Modal openElement={
-                                    <div className='size-8 grid place-content-center rounded-full border border-blue-500 text-blue-700 bg-blue-200 hover:bg-blue-500 hover:text-white hover:cursor-pointer'>
-                                        <EyeIcon className='size-4' />
-                                    </div>}>
-                                    <PostVer post={post} />
-                                </Modal>
+                        <div className='flex justify-center items-center gap-1'>
+
+                            <Modal openElement={
+                                <div className='size-8 grid place-content-center rounded-full border border-blue-500 text-blue-700 bg-blue-200 hover:bg-blue-500 hover:text-white hover:cursor-pointer'>
+                                    <EyeIcon className='size-4' />
+                                </div>}>
+                                <PostVer post={post} />
+                            </Modal>
+
+                            {post.is_draft
+                                ?
                                 <Modal openElement={
                                     <div className='size-8 grid place-content-center rounded-full border border-amber-500 text-amber-700 bg-amber-200 hover:bg-amber-500 hover:text-white hover:cursor-pointer'>
                                         <PencilIcon className='size-4' />
                                     </div>}>
                                     <PostModificar post={post} />
                                 </Modal>
+                                :
+                                <div className='size-8 grid place-content-center rounded-full border border-slate-500 text-slate-700 bg-slate-200'>
+                                    <PencilIcon className='size-4' />
+                                </div>
+                            }
+
+                            {post.is_draft
+                                ?
                                 <Modal openElement={
                                     <div className='size-8 grid place-content-center rounded-full border border-red-500 text-red-700 bg-red-200 hover:bg-red-500 hover:text-white hover:cursor-pointer'>
                                         <TrashIcon className='size-4' />
                                     </div>}>
                                     <PostEliminar post={post} />
                                 </Modal>
-                            </div>
-                        }
+                                :
+                                <div className='size-8 grid place-content-center rounded-full border border-slate-500 text-slate-700 bg-slate-200'>
+                                    <TrashIcon className='size-4' />
+                                </div>
+                            }
+
+                        </div>
                     </div>
                 ))}
-            </div>
+            </div >
         </>
     )
 }
